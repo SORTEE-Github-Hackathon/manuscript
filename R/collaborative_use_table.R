2c1ef4363130eec3e4aef5fbc2be6e1ee5f04883
Aariq <scottericr@gmail.com>
2022-03-09 10:03:16 -0500

    draft of table

---
+++ R/collaborative_use_table.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(here)
+df <- read_csv(here("R", "uses.csv"), na = "N/A")
+
+df
+
+# Table part --------------------------------------------------------------
+
+library(gt)
+library(glue)
+
+#function to replace difficulty with lightning bolts
+
+diff_to_lightning <- function(x) map_chr(x, ~rep("⚡️", .x) %>% glue_collapse())
+
+table <- 
+  df %>% 
+  gt() %>% 
+  text_transform(cells_body(difficulty), fn = diff_to_lightning) %>% 
+  # make NAs blanks
+  fmt_missing(example, missing_text = "") %>% 
+  #hide color column, for now
+  cols_hide(color) %>% 
+  cols_align("left") %>% 
+  #fix up column names
+  cols_label(
+    ways = "GitHub Use",
+    difficulty = "Technical Difficulty",
+    example = "Example"
+  ) %>% 
+  #add footer for difficulty legend
+  tab_footnote(
+    "⚡️ = Beginner ⚡️⚡️ = Intermediate ⚡️⚡️⚡️ = Advanced",
+    locations = cells_column_labels(
+      difficulty
+    )
+  ) %>% 
+  #try just using the color column
+  cols_unhide(color) %>% 
+  # text_transform(cells_body(color),grDevices::colors)
+  data_color(columns = color,
+             colors = color)
+  #would look better as a separate big arrow
+
+gtsave(table, here("content", "images", "collaborative_use.png"))
